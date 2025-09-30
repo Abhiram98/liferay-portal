68d5a57a50d0228bf58bb3d60e14343ee39c87d1
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-09-29 22:47:26 -0600

    seed rename for 123111

--- portal-impl/src/com/liferay/portal/spring/transaction/TransactionExecutorThreadLocal.java
+++ portal-impl/src/com/liferay/portal/spring/transaction/TransactionExecutorThreadLocal.java
@@ -17,14 +17,14 @@ public class TransactionExecutorThreadLocal {
 
 	public static TransactionExecutor getCurrentTransactionExecutor() {
 		Deque<TransactionExecutor> transactionExecutors =
-			_transactionExecutor.get();
+			_transactionExecutorDeque.get();
 
 		return transactionExecutors.peek();
 	}
 
 	protected static TransactionExecutor popTransactionExecutor() {
 		Deque<TransactionExecutor> transactionExecutors =
-			_transactionExecutor.get();
+			_transactionExecutorDeque.get();
 
 		return transactionExecutors.pop();
 	}
@@ -33,7 +33,7 @@ public class TransactionExecutorThreadLocal {
 		TransactionExecutor transactionExecutor) {
 
 		Deque<TransactionExecutor> transactionExecutors =
-			_transactionExecutor.get();
+			_transactionExecutorDeque.get();
 
 		transactionExecutors.push(transactionExecutor);
 	}
@@ -42,7 +42,7 @@ public class TransactionExecutorThreadLocal {
 	}
 
 	private static final ThreadLocal<Deque<TransactionExecutor>>
-		_transactionExecutor = new CentralizedThreadLocal<>(
+		_transactionExecutorDeque = new CentralizedThreadLocal<>(
 			TransactionExecutorThreadLocal.class + "._transactionExecutor",
 			ArrayDeque::new, false);
 
